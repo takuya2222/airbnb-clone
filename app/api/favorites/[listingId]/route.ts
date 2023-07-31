45bd0cd8df66dfff81a33583308931e4c55c28d8
takuya <hukujuth@gmail.com>
2023-07-31 23:49:56 +0900

    完成

---
+++ app/api/favorites/[listingId]/route.ts
@@ -0,0 +1,65 @@
+import { NextResponse } from "next/server";
+
+import getCurrentUser, { getSession } from "@/app/actions/getCurrentUser";
+import prisma from "@/app/libs/prismadb";
+
+interface IParams {
+  listingId?: string;
+}
+
+export async function POST(reqest: Request, { params }: { params: IParams }) {
+  const currentUser = await getCurrentUser();
+
+  if (!currentUser) {
+    return NextResponse.error();
+  }
+
+  const { listingId } = params;
+
+  if (!listingId || typeof listingId !== "string") {
+    throw new Error("Invalid ID");
+  }
+
+  let favoriteIds = [...(currentUser.favoriteIds || [])];
+
+  favoriteIds.push(listingId);
+
+  const user = await prisma.user.update({
+    where: {
+      id: currentUser.id,
+    },
+    data: {
+      favoriteIds,
+    },
+  });
+
+  return NextResponse.json(user);
+}
+
+export async function DELETE(reqest: Reqest, { params }: { params: IParams }) {
+  const currentUser = await getCurrentUser();
+
+  if (!currentUser) {
+    return NextResponse.error();
+  }
+  const { listingId } = params;
+
+  if (!listingId || typeof listingId !== "string") {
+    throw new Error("Invalid ID");
+  }
+
+  let favoriteIds = [...(currentUser.favoriteIds || [])];
+
+  favoriteIds = favoriteIds.filter((id) => id !== listingId);
+
+  const user = await prisma.user.update({
+    where: {
+      id: currentUser.id,
+    },
+    data: {
+      favoriteIds,
+    },
+  });
+
+  return NextResponse.json(user);
+}
---
+++ app/reservations/page.tsx
@@ -0,0 +1,42 @@
+import EmptyState from "../components/EmptyState";
+import ClientOnly from "../components/ClientOnly";
+import ReservationsClient from "./ReservationsClient";
+
+import getCurrentUser from "../actions/getCurrentUser";
+import getReservations from "../actions/getReservation";
+
+const ReservationsPage = async () => {
+  const currentUser = await getCurrentUser();
+
+  if (!currentUser) {
+    return (
+      <ClientOnly>
+        <EmptyState title="Unauthorized" subtitle="Please login" />
+      </ClientOnly>
+    );
+  }
+
+  const reservations = await getReservations({
+    authorId: currentUser.id,
+  });
+
+  if (reservations.length === 0) {
+    <ClientOnly>
+      <EmptyState
+        title="No reservation found"
+        subtitle="Looks like you have no reservations on you properties"
+      />
+    </ClientOnly>;
+  }
+
+  return (
+    <ClientOnly>
+      <ReservationsClient
+        reservations={reservations}
+        currentUser={currentUser}
+      />
+    </ClientOnly>
+  );
+};
+
+export default ReservationsPage;
